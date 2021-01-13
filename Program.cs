7d293ebe668a3d5e587825b925ac33d6252bc2df
Tyler L <43252312+TylerTheLoser@users.noreply.github.com>
2021-01-12 20:49:01 -0500

    Recalculation
    Still working on some groups recalculation and replacement, but 49 seems to be the most finished.

--- Program.cs
+++ Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.IO;
 
 namespace WireSplitter
@@ -13,6 +10,7 @@ namespace WireSplitter
         public string b { get; set; } //group header
         public string c { get; set; } //group trailer
         public string d { get; set; } //file trailer
+        public decimal e { get; set; } //temp placement for decimal
     }
     class Program
     {
@@ -27,6 +25,7 @@ namespace WireSplitter
                 string l;
                 string s = Directory.GetCurrentDirectory();
                 int p = 0; //payment count
+                List<string> ts = new List<string>();
                 Wire w = new Wire();
                 while((l = r.ReadLine()) != null)
                 {
@@ -36,30 +35,50 @@ namespace WireSplitter
                     } else if (l.Substring(0, 2) == "02") //group header
                     {
                         w.b = l + Environment.NewLine;
-                    } else if (l.Substring(0,2) == "98") //group trailer
+                    } else if (l.Substring(0, 2) == "98") //group trailer
                     {
+                        string[] h = { "02", "03", "16", "49", "88", "98" };
+                        string[] g = { "03" };
+                        var a = l.Substring((l.IndexOf(",") + 1));
+                        a = a.Substring(0, a.IndexOf(","));
                         w.c = l + Environment.NewLine;
                         for(int i = 1; i < p + 1; i++)
                         {
                             File.AppendAllText(s + @"\split_wire" + i + @".dat", w.c);
+                            Replace(a, ts[i - 1], (s + @"\split_wire" + i + @".dat"));
                         }
+                        //todo: number of accounts, all 03 records
+                        //todo: number of records, all 02, 03, 16, 49, 88, and 98 records
                     } else if(l.Substring(0,2) == "99") //file trailer
                     {
+                        string[] h = { "01", "02", "03", "16", "49", "98", "99" };
+                        string[] g = { "02" };
+                        var a = l.Substring((l.IndexOf(",") + 1));
+                        a = a.Substring(0, a.IndexOf(","));
                         w.d = l + Environment.NewLine;
                         for (int i = 1; i < p + 1; i++)
                         {
                             File.AppendAllText(s + @"\split_wire" + i + @".dat", w.d);
+                            Replace(a, ts[i -1], (s + @"\split_wire" + i + @".dat"));
                         }
+                        //todo: number of groups, the number of 02 records in the file
+                        //todo: number of records, total records, including this 99
                     } else if(l.Substring(0,2) == "03") //account identifier & summary status
                     {
                         p++;
+                        w.e = 0;
                         l = l + Environment.NewLine;
                         File.AppendAllText(s + @"\split_wire" + p + @".dat", w.a);
                         File.AppendAllText(s + @"\split_wire" + p + @".dat", w.b);
                         File.AppendAllText(s + @"\split_wire" + p + @".dat", l);
                     } else if(l.Substring(0,2) == "16") //transaction detail
                     {
-
+                        var a = l.Substring((l.IndexOf(",") + 1));
+                        a = a.Substring((a.IndexOf(",") + 1));
+                        a = a.Substring(0, a.IndexOf(","));
+                        a = a.Insert(a.Length - 2, ".");
+                        decimal b = decimal.Parse(a); //confirmed to be working.
+                        w.e = w.e + b;
                         l = l + Environment.NewLine;
                         File.AppendAllText(s + @"\split_wire" + p + @".dat", l);
                     } else if(l.Substring(0,2) == "88") //continuation record
@@ -68,11 +87,44 @@ namespace WireSplitter
                         File.AppendAllText(s + @"\split_wire" + p + @".dat", l);
                     } else if(l.Substring(0,2) == "49")
                     {
+                        string[] h = { "03", "16", "88", "49" };
+                        var a = l.Substring((l.IndexOf(",") + 1));
+                        a = a.Substring(0, a.IndexOf(","));
+                        var b = l.Substring((l.IndexOf(",") + 1));
+                        b = b.Substring(b.IndexOf(",") + 1);
+                        b = b.Substring(0, b.IndexOf('/'));
                         l = l + Environment.NewLine;
                         File.AppendAllText(s + @"\split_wire" + p + @".dat", l);
+                        ts.Add(w.e.ToString());
+                        int total = Count((s + @"\split_wire" + p + @".dat"), h);
+                        Replace(a, w.e.ToString(), (s + @"\split_wire" + p + @".dat"));
+                        Replace(b, total.ToString(), (s + @"\split_wire" + p + @".dat"));
                     }
                 }
             }
+        }
+        static void Replace(string x, string y, string file)
+        {
+            string z = File.ReadAllText(file);
+            y = y.Replace(".", ""); //get rid of the decimal point...
+            z = z.Replace(x, y);
+            File.WriteAllText(file, z);
+        }
+        static int Count(string file, string[] arr)
+        {
+            int total = 0;
+            var f = File.ReadLines(file);
+            foreach(var l in f)
+            {
+                foreach(var a in arr)
+                {
+                    if (l.Substring(0,2).Contains(a))
+                    {
+                        total++;
+                    }
+                }
+            }
+            return total;
         }
     }
 }
